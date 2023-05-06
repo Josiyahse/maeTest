e7ea26ca90127c5a53a8002b77bc0962ee4c9352
ismail <ismail95190@gmail.com>
2023-05-06 13:30:56 +0200

    wip faq

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
 import faq from "../../assets/iconFaq/faq.png";
-import { categories } from "./data";
+import { categories, faqWording } from "./data";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
-const Faq = () => {
+export function Accordions({ items }: any) {
+  const [openIndexes, setOpenIndexes] = useState<number[]>([]);
+
+  const handleClick = (index: any) => {
+    if (openIndexes.includes(index)) {
+      setOpenIndexes(openIndexes.filter((i) => i !== index));
+    } else {
+      setOpenIndexes([...openIndexes, index]);
+    }
+  };
+
+  return (
+    <div className="w-full  mx-auto">
+      {items.map((item: any, index: number) => (
+        <>
+          <div key={index}>
+            <div
+              className="flex justify-between items-center p-4 cursor-pointer"
+              onClick={() => handleClick(index)}
+            >
+              <h2
+                className={`text-lg font-bold ${
+                  openIndexes.includes(index) && "text-[#CE5729]"
+                }`}
+              >
+                {item.title}
+              </h2>
+
+              {openIndexes.includes(index) ? (
+                <AiOutlineMinus className="text-[#CE5729]" />
+              ) : (
+                <AiOutlinePlus />
+              )}
+            </div>
+            {openIndexes.includes(index) && (
+              <div className="p-4 bg-white">{item.content}</div>
+            )}
+          </div>
+          {index !== items.length - 1 && <hr className="border-3" />}
+        </>
+      ))}
+    </div>
+  );
+}
+
+export default function Faq() {
   const [activeFaq, setActiveFaq] = useState("services");
+
   return (
     <div className="flex flex-col justify-center m-[72px]">
       <div className="justify-center hidden md:flex ">
@@ -45,8 +92,14 @@ const Faq = () => {
           </div>
         ))}
       </div>
+      <div className=" flex mt-[100px] px-20 flex-col">
+        <h1 className="text-3xl font-bold">
+          {categories.find((cat) => cat.title === activeFaq)?.label}
+        </h1>
+        <Accordions
+          items={faqWording.filter((faqItem) => faqItem.category === activeFaq)}
+        />
+      </div>
     </div>
   );
-};
-
-export default Faq;
+}
